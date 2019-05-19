20989e857d484b99439a087548b17a03c4b11a85
Wiratmika <wiratmika@gmail.com>
2019-05-19 22:14:28 +0700

    Migrate Hero and About

---
+++ src/components/about.js
@@ -0,0 +1,47 @@
+import React from "react"
+import { StaticQuery, graphql } from "gatsby"
+import Img from "gatsby-image"
+
+export default ({ data }) => (
+  <section id="about">
+    <h2 class="text-center" data-magellan-destination="about">
+      About Me
+    </h2>
+    <a name="about" />
+    <div class="divider" />
+
+    <div class="row">
+      <div class="small-6 small-centered medium-3 medium-uncentered columns">
+        <StaticQuery
+          query={graphql`
+            query {
+              file(relativePath: { eq: "wiratmika.jpg" }) {
+                childImageSharp {
+                  fixed(width: 125, height: 125) {
+                    ...GatsbyImageSharpFixed
+                  }
+                }
+              }
+            }
+          `}
+          render={data => (
+            <Img
+              class="wiratmika"
+              fixed={data.file.childImageSharp.fixed}
+              alt="Wiratmika"
+            />
+          )}
+        />
+      </div>
+
+      <div class="small-12 medium-9 columns">
+        <p>
+          I'm a social impact-driven, full-stack technologist obsessed with
+          making disproportionate impact at scale. I speak both computer and
+          business languages. Would love to help building Indonesian tech
+          community and other engineering endeavors!
+        </p>
+      </div>
+    </div>
+  </section>
+)
---
+++ src/components/hero.js
@@ -0,0 +1,12 @@
+import React from "react"
+
+export default () => (
+  <section id="hello">
+    <div class="row">
+      <div class="small-12 small-centered columns">
+        <h1 class="text-center">Hello, there.</h1>
+        <p class="text-center">Thank you for your curiosity of me.</p>
+      </div>
+    </div>
+  </section>
+)
