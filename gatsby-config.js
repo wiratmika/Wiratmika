76d4207a62f945b8c18eea0ddd7dd9330c25d5d8
Wiratmika <wiratmika@gmail.com>
2019-05-30 12:18:49 +0700

    Set icon

--- gatsby-config.js
+++ gatsby-config.js
@@ -20,13 +20,13 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
+        name: `Wiratmika`,
+        short_name: `Wiratmika`,
         start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
-        display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        background_color: `#21ccbb`,
+        theme_color: `#21ccbb`,
+        display: `fullscreen`,
+        icon: `src/images/icon.png`,
       },
     },
     `gatsby-plugin-styled-components`,
